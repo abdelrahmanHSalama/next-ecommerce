8dcb7607bdabdb5cb20e16e14b29f1f8b95958e1
Abdelrahman Salama <Abdelrahman.H.Salama@outlook.com>
2025-04-25 01:43:37 +0300

    25042025_01

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,37 @@
-import React from "react";
+"use client";
+
+import { useState } from "react";
 import Link from "next/link";
 import Auth from "./Auth";
 import Navbar from "./Navbar";
 import Search from "./Search";
 
 const Header = () => {
+    const [hamburgerMenu, setHamburgerMenu] = useState(true);
     return (
-        <header className="w-full h-[80px] shadow-md flex justify-between lg:justify-center items-center">
-            <div className="lg:hidden absolute z-10 w-5/6 right-0 top-0 h-screen bg-white flex flex-col">
-                <button className="text-2xl font-bold ml-auto p-4">X</button>
-                <Navbar version="small" />
-                <Search />
-                <Auth />
+        <header className="w-full shadow-md flex flex-col lg:h-[80px] lg:flex-row lg:items-center">
+            <div className="lg:hidden w-full px-4 py-8 flex justify-between items-center mx-auto">
+                <Link href="/">
+                    <h1 className="text-2xl font-bold">NextShop</h1>
+                </Link>
+                <button
+                    className="text-2xl font-bold"
+                    aria-label={hamburgerMenu ? "Close menu" : "Open menu"}
+                    onClick={() => setHamburgerMenu(!hamburgerMenu)}
+                >
+                    {hamburgerMenu ? "X" : "☰"}
+                </button>
             </div>
-            <div className="w-5/6 flex items-center justify-between mx-auto">
+
+            {hamburgerMenu && (
+                <div className="lg:hidden flex flex-col gap-2 px-4 pb-2">
+                    <Navbar version="small" />
+                    <Search />
+                    <Auth />
+                </div>
+            )}
+
+            <div className="hidden lg:flex w-5/6 items-center justify-between mx-auto">
                 <Link href="/">
                     <h1 className="text-2xl font-bold">NextShop</h1>
                 </Link>
@@ -22,10 +40,6 @@ const Header = () => {
                     <Navbar version="full" />
                     <Search />
                     <Auth />
-                </div>
-
-                <div className="lg:hidden flex items-center">
-                    <button className="text-2xl font-bold">☰</button>
                 </div>
             </div>
         </header>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,21 +15,21 @@ const Navbar = ({ version }: { version: string }) => {
         <nav className={version === "small" ? "flex flex-col" : "flex gap-4"}>
             <Link
                 href="/products"
-                className="flex items-center gap-2 hover:bg-[#F5F5F5] transition duration-250 cursor-pointer p-2 rounded-md"
+                className="flex items-center gap-2 active:bg-[#F5F5F5] hover:bg-[#F5F5F5] transition duration-250 cursor-pointer p-2 rounded-md"
             >
                 <Icon icon="lucide:shopping-bag" width="20" height="20" />{" "}
                 Products
             </Link>
             <Link
                 href="/cart"
-                className="flex items-center gap-2 hover:bg-[#F5F5F5] transition duration-250 cursor-pointer p-2 rounded-md"
+                className="flex items-center gap-2 hover:bg-[#F5F5F5] active:bg-[#F5F5F5] transition duration-250 cursor-pointer p-2 rounded-md"
             >
                 <Icon icon="lucide:shopping-cart" width="20" height="20" /> Cart{" "}
                 {cartStateLength > 0 ? `(${cartStateLength})` : ""}
             </Link>
             <Link
                 href="/wishlist"
-                className="flex items-center gap-2 hover:bg-[#F5F5F5] transition duration-250 cursor-pointer p-2 rounded-md"
+                className="flex items-center gap-2 hover:bg-[#F5F5F5] active:bg-[#F5F5F5] transition duration-250 cursor-pointer p-2 rounded-md"
             >
                 <Icon icon="lucide:heart" width="20" height="20" /> Wishlist{" "}
                 {wishlistStateLength > 0 ? `(${wishlistStateLength})` : ""}
